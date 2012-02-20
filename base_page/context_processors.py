ee6eab7147a8b02b3b39b0be6bdb732db380aa2d
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-02-20 11:17:55 +0200

    context processor name modified to city_context

--- base_page/context_processors.py
+++ base_page/context_processors.py
@@ -4,10 +4,10 @@ that is needed to render the base pages
 """
 from models import CitySetting
 
-def base_page(request):
+def city_context(request):
     try:
         city_settings = CitySetting.on_site.all()[0]
     except IndexError:
         city_settings = {}
-    
+
     return {'city_settings': city_settings}
