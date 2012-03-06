77e32598db3e8bde3c8bf3755cdeb361caf64e03
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-03-06 11:26:28 +0200

    css for breadcrumbs added

--- base_page/admin.py
+++ base_page/admin.py
@@ -5,10 +5,10 @@ from models import CitySetting
 class CityLocation(admin.OSMGeoAdmin):
     pass
     '''
-    default_lon = 2407221.77716 
+    default_lon = 2407221.77716
     default_lat = 9123608.26437
     default_zoom = 12
     '''
-    
+
 admin.site.register(Feedback)
 admin.site.register(CitySetting, CityLocation)
