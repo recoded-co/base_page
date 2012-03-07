8134b8a5a7534f4165f4130a10beb57123ef573c
Pratik <pratik@Buzzinga.(none)>
2012-03-07 15:58:47 +0200

    Nothing really

--- base_page/admin.py
+++ base_page/admin.py
@@ -1,14 +1,12 @@
 from django.contrib.gis import admin
 from models import Feedback
 from models import CitySetting
-
+    
 class CityLocation(admin.OSMGeoAdmin):
-    pass
     '''
     default_lon = 2407221.77716 
     default_lat = 9123608.26437
     default_zoom = 12
-    '''
-    
+    '''    
 admin.site.register(Feedback)
 admin.site.register(CitySetting, CityLocation)
