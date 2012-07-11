80147504128ea02849f0d0da0fbe31391661550a
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-11 09:47:05 +0300

    setup version increased for next version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='base_page',
-    version='4.2.3',
+    version='4.2.4',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/base_page',
     packages=find_packages(),
