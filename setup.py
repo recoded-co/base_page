cee681740be94808a84eafba2ef05a16d134ba8d
Mikko Johansson <mikko.johansson@aalto.fi>
2012-09-14 12:52:37 +0300

    version number increased to 4.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='base_page',
-    version='4.3.0',
+    version='4.4.0',
     author='Kristoffer Snabb, Pratik Hublikar',
     url='https://github.com/geonition/base_page',
     packages=find_packages(),
